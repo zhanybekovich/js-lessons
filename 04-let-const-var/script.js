20e4c955fe862eda85a64e73e3767bbd3ca91c08
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2023-06-03 20:58:25 +0600

    Add lesson 4

---
+++ 04-let-const-var/script.js
@@ -0,0 +1,14 @@
+// variable that can be changed
+let age = 20;
+age = 21;
+
+// variable that can not be changed
+const birthYear = 1990;
+
+// this code cause error
+birthYear = 2023;
+const birthMonth;
+
+// legacy way of declaring variables
+var userName = 'John';
+
