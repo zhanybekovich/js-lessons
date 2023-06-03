4887591516d5d2ebf759fc6ebe5144b84210f99e
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2023-06-03 20:49:39 +0600

    Add lesson 3

---
+++ 03-data-types/script.js
@@ -0,0 +1,35 @@
+// === Primitive Types ===
+// Numbers
+let age = 23;
+let rate = 5.2;
+
+// Strings
+let firsName = "John";
+
+// Boolean
+let isValid = true;
+let hasCar = false;
+
+// Undefined
+let lastName;
+
+// Null
+let friends = null;
+
+// Symbol
+let mySymbol = Symbol("foo");
+
+// Big Integer
+let hugeNumber = 65465n;
+
+// Checking types
+console.log(typeof age);
+console.log(typeof rate);
+console.log(typeof firsName);
+
+// Dynamic typing
+let myFriend = "Pete";
+console.log(typeof myFriend);
+
+myFriend = 50;
+console.log(typeof myFriend);
