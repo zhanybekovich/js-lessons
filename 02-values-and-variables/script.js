72c989ab2fc4fed8e5ab564bb8bfc154ddf4ea25
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2023-06-03 19:19:05 +0600

    Add lesson 2

---
+++ 02-values-and-variables/script.js
@@ -0,0 +1,11 @@
+let firstName = "Mirlan";
+console.log(firstName);
+
+firstName = "John";
+console.log(firstName);
+
+// Conventions of naming Variables
+// -- use camelCase
+// -- can't start with numbers
+// -- don't use reserved keywords
+// -- use meaningful names
