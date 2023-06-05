e3e8d508c3c26395b57483beaae4c11046ecd876
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2023-06-05 14:47:23 +0600

    Add lesson 5

---
+++ 05-basic-operators/script.js
@@ -0,0 +1,60 @@
+// Math Operators
+console.log(40 + 10);
+console.log(200 - 45);
+console.log(4 * 2);
+console.log(10 / 3);
+console.log(2 ** 3);
+console.log(58 % 3);
+
+const firstName = "John";
+const lastName = "Smith";
+const fullName = firstName + " " + lastName;
+const fullName2 = `${firstName} ${lastName}`;
+console.log(fullName);
+console.log(fullName2);
+
+// Assignment Operators
+let a = 10;
+a = a + 2;
+console.log(a);
+
+let b = 10;
+b += 2;
+console.log(b);
+
+let c = 10;
+c -= 3;
+console.log(c);
+
+let d = 10;
+d *= 20;
+console.log(d);
+
+let e = 10;
+e /= 3;
+console.log(e);
+
+let f = 200;
+f %= 3;
+console.log(f);
+
+// Increment
+let g = 20;
+g++;
+console.log(g);
+
+// Decrement
+let h = 50;
+h--;
+console.log(h);
+
+// Comparison Operators
+let i = 100;
+let j = 99;
+console.log(i > j);
+console.log(i >= j);
+
+console.log(i < j);
+console.log(i <= j);
+console.log(i == j);
+console.log(j === i);
