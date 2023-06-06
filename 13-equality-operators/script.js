ed3030116d8449912261a00d00cf848f610847a2
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2023-06-06 20:46:38 +0600

    Add lesson 13

---
+++ 13-equality-operators/script.js
@@ -0,0 +1,46 @@
+const age = 18;
+
+if (age === 18) {
+  console.log("Adult");
+} else {
+  console.log("Not an adult");
+}
+
+const age2 = "18";
+if (age2 == 18) {
+  console.log("Adult 2");
+} else {
+  console.log("Not anadult 2");
+}
+
+if (age2 === 18) {
+  console.log("Adult 3");
+} else {
+  console.log("Not an Adult 3");
+}
+
+const userAge = prompt("Enter your favorite number: ");
+console.log(typeof userAge);
+if (userAge == 18) {
+  console.log("User is an adult");
+} else {
+  console.log("User is not an adult");
+}
+
+if (userAge === 18) {
+  console.log("User is an adult");
+} else {
+  console.log("User is not an adult");
+}
+
+if (userAge !== 18) {
+  console.log("User is not an adult");
+} else {
+  console.log("User is not an adult");
+}
+
+if (userAge != 18) {
+  console.log("User is not an adult");
+} else {
+  console.log("User is not an adult");
+}
