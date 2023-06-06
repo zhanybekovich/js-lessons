8eb624f926182c300d8f1ce60aaf1bf05076c272
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2023-06-06 22:05:10 +0600

    Add lesson 14

---
+++ 14-boolean-logic/script.js
@@ -0,0 +1,11 @@
+const age = 45;
+
+if (age > 0 && age < 3) {
+  console.log("Child");
+} else if (age > 4 && age < 14) {
+  console.log("Teen");
+} else if (age > 15 && age < 35) {
+  console.log("Young");
+} else {
+  console.log("Adult");
+}
