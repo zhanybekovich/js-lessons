2ed0146e775c3210330dcd7752e602aaa18a5177
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2023-06-07 21:37:28 +0600

    Add lesson 1

---
+++ 02-fundamentals-2/01-strict-mode/index.js
@@ -0,0 +1,12 @@
+"use strict";
+
+let hasDriversLicense = false;
+const passTest = true;
+
+if (passTest) {
+  hasDriverLicense = true; // without strict mode this code will not cause errors
+  console.log("I can drive");
+}
+
+const interface = "Hello";
+console.log(interface);
