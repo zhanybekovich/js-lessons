a342a605a0c3cf72d38fc3ee48555c3a1d1f3686
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2023-06-07 15:18:57 +0600

    Add lesson 18

---
+++ 18-expression-statement/script.js
@@ -0,0 +1,10 @@
+// expressions --> produce values
+2 + 1;
+true && false && true;
+
+// statements --> do not produce values
+if (23 > 10) {
+  const str = "23 is bigger than 10";
+}
+
+// !!! in template literals use only expressions
