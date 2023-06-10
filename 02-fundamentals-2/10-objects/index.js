66ffa1526b7c754c298850d71cacffaa8407b3d3
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2023-06-10 21:02:50 +0600

    Add lesson 10

---
+++ 02-fundamentals-2/10-objects/index.js
@@ -0,0 +1,11 @@
+const john = {
+  firstName: "John",
+  lastName: "Smith",
+  age: new Date().getFullYear() - 1997,
+  job: "Developer",
+  friends: ["Mike", "Bob", "Brad"],
+};
+
+console.log(john.firstName);
+console.log(john.lastName);
+console.log(john.age);
