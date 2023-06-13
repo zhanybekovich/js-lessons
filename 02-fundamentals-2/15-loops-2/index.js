67c2164fd54a3ecda053469f11ee905441e6078e
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2023-06-13 18:49:47 +0600

    Update lesson 15

--- 02-fundamentals-2/15-loops-2/index.js
+++ 02-fundamentals-2/15-loops-2/index.js
@@ -10,3 +10,9 @@ for (let i = friends.length; i > 0; i--) {
 }
 
 console.log(friends);
+
+for (let i = 1; i <= 9; i++) {
+  for (let j = 1; j <= 10; j++) {
+    console.log(`${i} * ${j} = ${i * j}`);
+  }
+}
