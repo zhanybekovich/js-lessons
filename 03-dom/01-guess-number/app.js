6df06c9097cec3a8266ce315d8f11d93df4c35c6
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2023-06-15 21:50:03 +0600

    Add guess game: DOM manipulation

---
+++ 03-dom/01-guess-number/app.js
@@ -0,0 +1,5 @@
+// find element
+document.querySelector(".message").textContent = "Correct!";
+document.querySelector(".number").textContent = 14;
+document.querySelector(".score").textContent = 45;
+document.querySelector(".guess").value = 50;
