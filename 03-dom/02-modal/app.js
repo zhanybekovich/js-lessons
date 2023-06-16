4c0bc3da5db71288d85e27b760fd9c1e79a466f8
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2023-06-16 22:45:10 +0600

    Add modal project

---
+++ 03-dom/02-modal/app.js
@@ -0,0 +1,22 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const modalClose = document.querySelector(".modal-close");
+const btnsOpenModal = document.querySelectorAll(".show-modal");
+
+function openModal() {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+}
+
+function closeModal() {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+}
+
+for (let i = 0; i < btnsOpenModal.length; i++) {
+  btnsOpenModal[i].addEventListener("click", openModal);
+}
+
+modalClose.addEventListener("click", closeModal);
+
+overlay.addEventListener("click", closeModal);
