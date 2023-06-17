75ab9df0058a9c92d5a1f65f9b2b38b44649e0ab
Mirlan Urzhanov <urzhanovmirlan@gmail.com>
2023-06-17 21:14:59 +0600

    Add player switch

--- 03-dom/03-pig-game/app.js
+++ 03-dom/03-pig-game/app.js
@@ -9,12 +9,17 @@ const btnRoll = document.querySelector(".btn--roll");
 const btnHold = document.querySelector(".btn--hold");
 const current0El = document.getElementById("current--0");
 const current1El = document.getElementById("current--1");
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 
 // Start values
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add("hidden");
 let currentScore = 0;
+let activePlayer = 0;
+const scores = [0, 0];
+player0El.classList.add("player--active");
 
 // Rolling dice
 btnRoll.addEventListener("click", function () {
@@ -29,8 +34,14 @@ btnRoll.addEventListener("click", function () {
   if (dice !== 1) {
     // add dice to current score
     currentScore += dice;
-    current0El.textContent = currentScore;
+    document.getElementById(`current--${activePlayer}`).textContent =
+      currentScore;
   } else {
     // switch to next player
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1 : 0;
+    player0El.classList.toggle("player--active");
+    player1El.classList.toggle("player--active");
   }
 });
